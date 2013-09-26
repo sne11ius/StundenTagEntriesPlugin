202cc8ccef0e2ef5fa86f26cf7e25852e954dbf2
Cornelius Lilge <cornelius.lilge@gmail.com>
2013-09-26 22:05:20 +0200

    Initial commit

---
+++ src/main/java/nu/wasis/stunden/plugins/tagentries/config/StundenTagEntriesPluginConfiguration.java
@@ -0,0 +1,13 @@
+package nu.wasis.stunden.plugins.tagentries.config;
+
+import java.util.Map;
+
+public class StundenTagEntriesPluginConfiguration {
+
+	private Map<String, String> tagAssociations;
+
+	public Map<String, String> getTagAssociations() {
+		return tagAssociations;
+	}
+	
+}
